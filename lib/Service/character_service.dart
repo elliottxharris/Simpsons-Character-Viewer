5d39b364a322af74be40a6119c63f76334d466aa
elliottxharris <elliottxharris@gmail.com>
2023-12-15 20:38:45 -0600

    add service

---
+++ lib/Service/character_service.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+class CharacterService {
+  Future<Map<String, dynamic>?> getCharacters() async {
+    final url = Uri.parse('http://api.duckduckgo.com/?q=simpsons+characters&format=json');
+
+    final res = await http.get(url);
+
+    if (res.statusCode == 200) {
+      return jsonDecode(res.body);
+    }
+
+    return null;
+  }
+}
