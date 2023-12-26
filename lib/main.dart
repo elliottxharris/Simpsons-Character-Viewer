cf14e072623f1b040203848bfb18bc8ab25b68ae
elliottxharris <30444461+elliottxharris@users.noreply.github.com>
2023-12-26 10:12:53 -0600

    Update title

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Anywhere Excercise',
+      title: 'Simpsons Character Viewer',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
